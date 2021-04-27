5f8d2edf0b1ded6aa0fc922fdde747d874e2ecad
JenniferFields <jennifer.fields.855@my.csun.edu>
2021-04-26 22:11:17 -0700

    editing community comp code

--- Scripts/TidePoolCommunityComposition.R
+++ Scripts/TidePoolCommunityComposition.R
@@ -14,11 +14,15 @@ source("scripts/tidepoolphysicalparameters.R")
 #data
 Sessiles <- read_csv("Data/CommunityComposition/SessilesAll.csv")
 Mobiles <- read_csv("Data/CommunityComposition/Mobiles.csv")
-SessilesGroupings<-read_csv("Data/CommunityComposition/SessilesFunwithcitations.csv")
-MobileGroupings<-read_csv("Data/CommunityComposition/MobilesFun.csv")
+SessilesGroupings<-read_csv("Data/CommunityComposition/SessilesFun_withcitations.csv")
+MobileGroupings<-read_csv("Data/CommunityComposition/MobilesFun_withcitations.csv")
 
 #replace NA with 0
 Mobiles[is.na(Mobiles)]<-0
+#remove immediate time pt
+Mobiles<-Mobiles %>%
+  filter(Before_After !="Immediate")
+#write.csv(Mobiles,file="Data/CommunityComposition/Mobilespplist.csv") #save spp list without mid point sampling
 
 #######Sessiles#########
 #convert characters to numeric in sessile sheet
@@ -36,12 +40,49 @@ StandardizedSessile<- 100*PercentSessile/rowSums(PercentSessile)
 
 Communitymetrics<-cbind(Sessiles$PoolID, Sessiles$Foundation_spp, Sessiles$Removal_Control, 
                         Sessiles$Before_After,StandardizedSessile,PercentSessile$Mytilus.californianus,PercentSessile$Phyllospadix.spp)
+
+#adjusted mussel and surfgrass are made relative sum of the total cover of the pool whereas Surfgrass and Mussel cover are not by size
 Communitymetrics <- Communitymetrics %>%
   rename(PoolID = "Sessiles$PoolID", Foundation_spp = "Sessiles$Foundation_spp",Removal_Control ="Sessiles$Removal_Control",
-         Before_After ="Sessiles$Before_After", AdjMusselCover=Mytilus.californianus, AdjSurfgrassCover=Phyllospadix.spp, MusselCover = "PercentSessile$Mytilus.californianus",SurfgrassCover = "PercentSessile$Phyllospadix.spp")
+         Before_After ="Sessiles$Before_After",
+         MusselCover = "PercentSessile$Mytilus.californianus",SurfgrassCover = "PercentSessile$Phyllospadix.spp") %>% #mussel and surfgrass cover are not standardized to pool 
+  dplyr::filter(Before_After != "Immediate")
 #rename joined columns
 
-#summarized groups of sessile community
+#write.csv(Communitymetrics,file="Data/CommunityComposition/Sessilesspplist.csv") #save updated spp list without mid point sampling
+
+#merge with functional groups
+###phylum, class and functional group
+SessilesFun<-Communitymetrics %>%
+  dplyr::group_by(PoolID, Foundation_spp, Before_After, Removal_Control) %>%
+  tidyr::pivot_longer(
+    cols = Mytilus.californianus:Stylantheca.spp, #leave out unadjusted surfgrass and mussel cover
+    names_to = "Species", #creates column with species in longformat
+    values_to = "Cover", #adds column for % cover
+    values_drop_na = TRUE
+  ) 
+
+SessilesFun<-left_join(SessilesFun,SessilesGroupings) #combine with fun groups 
+write.csv(SessilesFun,file="Data/CommunityComposition/Sessilefunctionalgroups.csv")
+
+MobilesFun<-Mobiles %>%
+  dplyr::filter(Before_After != 'Immediate') %>%
+  dplyr::group_by(PoolID, Foundation_spp, Before_After, Removal_Control) %>%
+  tidyr::pivot_longer(
+    cols = Nuttalina.spp:Gunnel,
+    names_to = "Species", #creates column with species in longformat
+    values_to = "Count", #adds column for % cover
+    values_drop_na = TRUE
+  ) 
+
+MobilesFun<-left_join(MobilesFun,MobileGroupings)
+
+#save spp with functional group categories
+#write.csv(Mobiles,file="Data/CommunityComposition/Mobilespp.csv")
+#write.csv(MobilesFun,file="Data/CommunityComposition/Mobilefunctionalgroups.csv")
+
+
+#summarized groups of sessile community shift between before and after
 #This is TP Sessile Community Metrics Dataset in Data folder
 Summarizedgroups<-Communitymetrics %>%
   dplyr::filter(Before_After != "Immediate") %>%
@@ -78,7 +119,7 @@ Funsppcover<- Communitymetrics%>%
   summarise(Mytilusdelta = -1*(MusselCover[Before_After == 'After'] - MusselCover[Before_After == 'Before']),
             Phyllodelta = -1*(SurfgrassCover[Before_After == 'After'] - SurfgrassCover[Before_After == 'Before']))
 
-
+#average between time periods for physical parameters
 PP<-TidePooldes %>%
   dplyr::group_by(PoolID,Removal_Control) %>%
   dplyr::summarise(SAVav = mean(SAtoV), #ave between before and after since SA/V changed with fspp removal
--- Scripts/combinedmicrobedatasets.R
+++ Scripts/combinedmicrobedatasets.R
@@ -2,9 +2,9 @@ library(tidyverse)
 
 #load data to merge
 
-FCM_Fixed_clean <- read_csv("Data/Nyssa_FCM_Fixed_clean.csv")
-Tidepools_fDOM_Final <- read_csv("Data/Silbiger_Tidepools_fDOM_Final.csv")
+FCM_Fixed_clean <- read_csv("Data/Microbe_Clean/Nyssa_FCM_Fixed_clean.csv")
+Tidepools_fDOM_Final <- read_csv("Data/Microbe_Clean/Silbiger_Tidepools_fDOM_Final.csv")
 
 combinedFCMandfDOMdata<-left_join(FCM_Fixed_clean,Tidepools_fDOM_Final)
 
-write.csv(combinedFCMandfDOMdata, file = 'Data/combined_FCMandfDOMdata.csv')
+write.csv(combinedFCMandfDOMdata, file = 'Data/Microbe_Clean/combined_FCMandfDOMdata.csv')
