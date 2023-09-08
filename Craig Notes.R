15442e69f049d0981686526f5e2e49ed3506f9d2
Nyssa Silbiger <nyssa.silbiger@csun.edu>
2023-09-08 08:30:50 -0700

    add notes

---
+++ Craig Notes.R
@@ -0,0 +1,54 @@
+
+
+### Procclhlorocoocous has chl a... not in this study... mostly offshore and tropical
+### synnecocochous also has chl a and phycoarithrym and is bigger than proc and is abundant coastally... bacteria 
+## auto pico eukaryotes... not P or S and have chlorophyll... any other phytoplankton... probably not bacteria... mostly phytoplankton.. maybe 3-5 dominat species
+# heterotophic bacteria are a lot more 
+
+## Look up Karen Self.. FCM wizard
+
+## FCM will not capture big and/or rare phytoplankton
+
+## phyto generation times 
+## heterotrophic baterica are mostly small and slow growing with generation times on the order of days.. probably being eaten... but this is a lot of genera
+## heterotrophic bacteria are thousands of different species. So some of these may have short generation times... but bigger ones probably slow growin6
+# oligio trophs grow slow... copitrophs are more R selected species.  Have big genomes and ramp up generation times to hours.. If the H bac went up it might be copiotrophic and growing fast in response to the pulse of nutrients
+## if they don't change it could be the oligiotrophs that have slow generation times
+# Pico auto... look up generation times to see how fast they grow... in the lab they have generation times on the order of hours
+## when upwelling is occuring the phytoplankton are often primed to double really fast
+
+## maybe go back to the FCM 
+
+## Synnecocochus could divide at certain times of the day more than at night
+
+# Find a review article in nature microbiology or New phytology on current understanding of physio of synnenocochus, etc... does it use ammonium or nitrate etc.
+
+## upwelling primes the system.. so july and august may be different because of how the system was primed
+## show histograms of nutrients and temperature to show july vs august to say if it was or wasnt upwelling
+
+### what do mussels eat??? And how fast do they consume it??? Think about the rate at which they eat to think of rate of removal versus production.
+
+## In H Back rate... the grazing by mussels outweighs grazing in surfgrass pools so thats why the rates are lower 
+## always think there is both grazing and growth happening at the same time
+
+
+# G. Yahel... isreali scientist that has worked on filter feeders like sponges and tunicates and has super detailed numbers on how fast certain critters consume bacteria
+
+## different invertebrates are picky about the size of cells that they eat.  What is the size range of cells that mussels eat.
+### with fDOM look at the rate of removal with each components. Keep the fDOM separate initially and pool if there are similar patterns
+## can go a long way with the fluxes of fDOM... think about a community uptake 
+
+### consumption and production of microbes and organic patterns in tide pools... think about the removals too... 
+### Maybe think about dropping day vs night
+## write for ecology letters maybe
+### think about mussels as a massive grazer
+## H Bac love eating diatom exudates
+
+## can also focus on just one species... for example, with and without mussels... there is a massive diatom bloom (check this in the pico auto data)
+## focusing on flux.... this is the gold.. It is "net" removal or addition of cells
+
+### Most interested in the removal vs control pools and maybe upwelling ideas... 
+## producer vs consumer-dominated pools... differs with and without the benthic dominant organism
+## look at regressions of fluxes change.. ... this could be compelling.. nutrient fluxes vs microbe count fluxes
+### maybe leave out the day and night 
+ 
